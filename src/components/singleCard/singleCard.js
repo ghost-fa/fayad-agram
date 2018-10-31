2a6fd80f4cecbcdf47ca0235406c5b3900920859
ghost-fk <fayyad_r22@yahoo.com>
2018-10-31 16:39:37 +0100

    fix the error

--- src/components/singleCard/singleCard.js
+++ src/components/singleCard/singleCard.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-
+import './singleCard.css';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-
+import { connect } from 'react-redux';
+import { addComment, removeComment } from '../../store/actions/actions';
+import commentData from '../../data/comments';
 import Avatar from '@material-ui/core/Avatar';
-
+import postData from '../../data/post.js';
 import Typography from '@material-ui/core/Typography';
 import red from '@material-ui/core/colors/red';
-import InteractiveIcons from '../UI/interactiveIcons';
+// import InteractiveIcons from '../UI/interactiveIcons';
 import { Link } from 'react-router-dom';
 import PostData from '../../data/post.js';
 const styles = theme => ({
   card: {
-    maxWidth: 400
+    maxWidth: 850
   },
   media: {
     height: 0,
@@ -49,44 +51,74 @@ class RecipeReviewCard extends React.Component {
     this.setState(state => ({ expanded: !state.expanded }));
   };
 
+  onchange = e => {
+    e.preventDefault();
+    const updateState = {
+      ...this.state
+    };
+    updateState.value = e.target.value;
+    this.setState({ value: updateState.value });
+  };
+
   render() {
     const post = PostData[this.props.match.params.id];
 
     const { classes } = this.props;
     return (
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="Recipe" className={classes.avatar}>
-              F
-            </Avatar>
-          }
-          title="Shrimp and Chorizo Paella"
-        />
-        <CardMedia
-          className={classes.media}
-          image={post.src}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography component="p" />
-          {post.caption}
-        </CardContent>
-
-        <InteractiveIcons>
-          <i className="material-icons">favorite_border</i>
-        </InteractiveIcons>
-
-        <div className="cardIcon">
-          <Link to="/view/commint">
-            <InteractiveIcons>
+      <div>
+        <Card className={classes.card}>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="Recipe" className={classes.avatar}>
+                F
+              </Avatar>
+            }
+            title="Shrimp and Chorizo Paella"
+          />
+          <CardMedia
+            className={classes.media}
+            image={post.src}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography component="p" />
+            {post.caption}
+          </CardContent>
+          <i
+            className="material-icons"
+            onClick={() => {
+              this.props.increment();
+            }}
+          >
+            favorite_border{' '}
+          </i>{' '}
+          {post.likes}
+          <div className="cardIcon">
+            <Link to="/view/commint">
               <i className="material-icons">mode_comment</i>
-            </InteractiveIcons>
-          </Link>
-        </div>
-      </Card>
+            </Link>
+          </div>
+        </Card>
+        <input type="text" onChange={e => this.onchange(e)} />
+        <button onClick={() => this.props.addComment(this.state.value)}>
+          add
+        </button>
+      </div>
     );
   }
 }
 
-export default withStyles(styles)(RecipeReviewCard);
+const storToProps = store => {
+  return {
+    comment: store.comment
+  };
+};
+const actionToProps = {
+  addComment: addComment,
+  removeComment: removeComment
+};
+const respewithStyle = withStyles(styles)(RecipeReviewCard);
+export default connect(
+  storToProps,
+  actionToProps
+)(respewithStyle);
